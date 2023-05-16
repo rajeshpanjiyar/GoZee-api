b70e509b3cafe28d94d28e5ba2c87bca4c1b98e8
Rajesh Panjiyar <rajeshpanjiyar12@gmail.com>
2023-05-16 12:33:40 +0530

    fixed the configuration for file-upload

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 const express = require("express");
 const app = express();
-const fileUpload = require('express-fileupload')
+const fileUpload = require("express-fileupload");
 const port = process.env.PORT || 4000;
 var cors = require("cors");
 const connectDb = require("./Db/db");
 app.use(cors());
-app.use(fileUpload({
-  useTempFiles: true
-}))
+app.use(
+  fileUpload({
+    useTempFiles: true,
+    tempFileDir: path.join(__dirname, "tmp/"),
+    abortOnLimit: true,
+    preserveExtension: true,
+    safeFileNames: true,
+    limits: { fieldSize: 50 * 2024 * 1024 },
+  })
+);
 app.use(express.json());
 app.use("/api/cars/", require("./Routes/carsRoutes"));
 app.use("/booking/api/cars/", require("./Routes/carsRoutes"));
@@ -15,7 +22,6 @@ app.use("/editcar/api/cars/", require("./Routes/carsRoutes"));
 app.use("/api/users/", require("./Routes/usersRoutes"));
 app.use("/booking/api/bookings/", require("./Routes/bookingsRoute"));
 app.use("/api/bookings/", require("./Routes/bookingsRoute"));
-
 
 app.listen(port, () => {
   console.log(`Server is running at port: ${port} `);
